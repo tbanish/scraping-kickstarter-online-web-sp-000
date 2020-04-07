fdca61fef972bf7100fcf87a21aa40d3706756d8
Tom Banish <tombanish@gmail.com>
2020-04-07 17:19:17 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -23,8 +23,8 @@ def create_project_hash
   projects = {}
   
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
-  end
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
 end
 
 create_project_hash
