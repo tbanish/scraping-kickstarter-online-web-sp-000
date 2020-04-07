10f20796399e118d76f6b47a0bf453de3f9da394
Tom Banish <tombanish@gmail.com>
2020-04-07 17:13:18 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,7 @@
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta").text
+# perenct funded: project.css("ul.project-stats li.first.funded strong").text
 
 
 require "nokogiri"
