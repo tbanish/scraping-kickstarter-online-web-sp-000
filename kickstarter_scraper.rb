9398b0a8634f0178b97e19b31f5fc68c93ef3608
Tom Banish <tombanish@gmail.com>
2020-04-07 16:50:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,5 +3,7 @@ require "nokogiri"
 require "pry"
 
 def create_project_hash
-  # write your code here
+  le.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
